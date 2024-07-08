074e89aa28756a2be6748b4ed3891f694a2c82bf
nihad1213 <nihad.nemetli@gmail.com>
2024-07-08 17:34:38 +0400

    index.php part and path created

--- api/index.php
+++ api/index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Hello World";
+// Create varible. This variable is equals to our URI
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+// Explode URI for `/`
+$parts = explode("/", $path);
+
+$resource = $parts[3];
+// If there is no Id $id will be null
+$id = $parts[4] ?? null;
+
+
