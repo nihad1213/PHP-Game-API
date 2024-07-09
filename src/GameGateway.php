24f39a7f2ecd7f836da46653a41b97a769f32d62
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-07-09 20:19:27 +0400

    Update GameGateway.php

--- src/GameGateway.php
+++ src/GameGateway.php
@@ -47,4 +47,47 @@ class GameGateway {
 
         return $data;
     } 
+
+    /**
+     * Add data to database
+     * @param array $data
+     * @return string
+     */
+    public function create(array $data): string {
+        
+        $sql = "INSERT INTO `games` 
+                (`title`, `genre`, `platform`, `developer_ID`, `publisher_ID`, `release_data`, `status`, `rating`, `price`, `description`, `cover_image`, `created_at`, `updated_at`) 
+                VALUES 
+                (:title, :genre, :platform, :developer_ID, :publisher_ID, :release_data, :status, :rating, :price, :description, :cover_image, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)";
+    
+        $stmt = $this->conn->prepare($sql);
+    
+        // Avoiding from error: Array to string
+        $platformJson = json_encode($data['platform']);
+    
+        // Bind values to placeholders
+        $stmt->bindValue(':title', $data['title'], PDO::PARAM_STR);
+        $stmt->bindValue(':genre', $data['genre'], PDO::PARAM_STR);
+        $stmt->bindValue(':platform', $platformJson, PDO::PARAM_STR);
+        $stmt->bindValue(':developer_ID', $data['developer_ID'], PDO::PARAM_INT);
+        $stmt->bindValue(':publisher_ID', $data['publisher_ID'], PDO::PARAM_INT);
+        $stmt->bindValue(':release_data', $data['release_data'], PDO::PARAM_STR);
+        $stmt->bindValue(':status', $data['status'], PDO::PARAM_BOOL);
+        $stmt->bindValue(':rating', $data['rating'], PDO::PARAM_STR);
+        $stmt->bindValue(':price', $data['price'], PDO::PARAM_STR);
+        $stmt->bindValue(':description', $data['description'], PDO::PARAM_STR);
+        $stmt->bindValue(':cover_image', $data['cover_image'], PDO::PARAM_STR);
+    
+        if (empty($data['status'])) {
+            $stmt->bindValue(":status", null, PDO::PARAM_NULL);
+        } else {
+            $stmt->bindValue(":status", $data['status'], PDO::PARAM_INT);
+        }
+    
+        $stmt->execute();
+    
+        return $this->conn->lastInsertId();
+    }
+    
+    
 }
