dbc8e8e2689ddb4df686281c33c52d9d0c96952e
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-07-25 22:00:45 +0400

    Update data.sql

--- data/data.sql
+++ data/data.sql
@@ -30,21 +30,21 @@ CREATE TABLE publishers (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE games(
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `title` VARCHAR(256) NOT NULL,
-    `genre` VARCHAR(256),
-    `platform` JSON,
-    `developer_ID` INT,
-    `publisher_ID` INT,
-    `release_data` DATE,
-    `status` BOOLEAN,
-    `rating` VARCHAR(10),
-    `price` DECIMAL(10, 2),
-    `description` TEXT,
-    `cover_image` VARCHAR(255),
-    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+CREATE TABLE games (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(256) NOT NULL,
+    genre VARCHAR(256),
+    platform JSON,
+    developer_ID INT,
+    publisher_ID INT,
+    release_date DATE,
+    status BOOLEAN,
+    rating VARCHAR(10),
+    price DECIMAL(10, 2),
+    description TEXT,
+    cover_image VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (developer_ID) REFERENCES developers(id),
     FOREIGN KEY (publisher_ID) REFERENCES publishers(id)
 );
@@ -53,7 +53,7 @@ CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(127) NOT NULL,
     username VARCHAR(126) NOT NULL,
-    password_hash VARCHAR(255) NOT NULL
+    password_hash VARCHAR(255) NOT NULL,
     api_key VARCHAR(32) NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (username),
